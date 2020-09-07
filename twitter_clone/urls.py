25392d4f7f181b4257984a66366517c88ce600a8
Davi Félix <davi.felix.lima@gmail.com>
2020-09-07 11:39:35 -0300

    Serving static files for development

--- twitter_clone/urls.py
+++ twitter_clone/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
@@ -25,3 +27,7 @@ urlpatterns = [
     path('api/tweets/', include('tweets.urls')),
     re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
